ed07de1def9120cb8fb05410eac9de159970555f
muskangera411 <muskangera411@gmail.com>
2025-01-21 12:38:05 +0530

    Solution to Determine Eligibility for Voting

--- Determine Eligibility for Voting@@6788dd63490637cfc44e1b99/code.c
+++ Determine Eligibility for Voting@@6788dd63490637cfc44e1b99/code.c
@@ -3,7 +3,7 @@ int main() {
     int age;
     printf("");
     scanf("%d",&age);
-    if(age>+18){printf("Eligible");}
+    if(age>=18){printf("Eligible");}
     else{printf("Not Eligible");}
     return 0;
 }
