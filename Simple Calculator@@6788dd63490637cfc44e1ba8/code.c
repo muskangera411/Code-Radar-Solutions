7cc03f52aa27aff7b94872a44fa5944ef4c987ce
muskangera411 <muskangera411@gmail.com>
2025-02-04 18:33:38 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -8,7 +8,11 @@ int main(){
     if(c=='+'){printf("%d",a+b);}
     else if(c=='-'){printf("%d",a-b);}
     else if(c=='*'){printf("%d",a*b);}
-    else if(c=='/'){printf("%d",a/b);}
-    else{printf("Error");}
+    else if(c=='/'){if(b==0){
+        printf("error");
+        else{printf("%d",a/b);}
+
+    }}
+    else{printf("error");}
     return 0;
 }
