62c4578959ba0068fa3952c10eb49b19a0c28c5d
muskangera411 <muskangera411@gmail.com>
2025-02-04 11:46:57 +0530

    Solution to Check if Array is Sorted

--- Check if Array is Sorted@@676c02002dbc8c3e7a280b6f/code.c
+++ Check if Array is Sorted@@676c02002dbc8c3e7a280b6f/code.c
@@ -6,11 +6,11 @@ int main(){
     for(int i=0;i<a;i++){
         scanf("%d",&arr[i]);
     }
-    int sort=0;
+    int sort=1;
     for(int i=0;i<a-1;i++){
-        if(arr[i]<=arr[i+1]){sort=1;}
-        else if(arr[i]>=arr[i+1]){sort=0;}
-    }if(sort==0){printf("Not Sorted");}
-    else{printf("Sorted");}
+        if(arr[i]>arr[i+1]){sort=0;break;}
+        
+    }if(sort==0){printf("Not Sorted\n");}
+    else{printf("Sorted\n");}
     return 0;
 }
