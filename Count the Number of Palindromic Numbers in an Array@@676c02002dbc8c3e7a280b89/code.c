acbb0a0b8cbeabcdd01e9347bcddcb4eb9486f6f
Muskan Gera <muskangera411@gmail.com>
2025-03-11 19:18:25 +0530

    Solution to Count the Number of Palindromic Numbers in an Array

--- Count the Number of Palindromic Numbers in an Array@@676c02002dbc8c3e7a280b89/code.c
+++ Count the Number of Palindromic Numbers in an Array@@676c02002dbc8c3e7a280b89/code.c
@@ -1,34 +1,33 @@
 #include <stdio.h>
-int is_palindrome(int num);
-int main(){
-    int a;
-    scanf("%d",&a);
-    int arr[a];int count=0;
-    for(int i=0;i<a;i++){
-        scanf("%d",&arr[i]);
-    
-    if(is_palindrome(arr[i])){
-        count++;
-    }}
-    printf("%d",count);
-    return 0;
 
+int is_palindrome(int num);
+
+int main() {
+    int a, count = 0;
+    scanf("%d", &a);
+    int arr[a];
+
+    for (int i = 0; i < a; i++) {
+        scanf("%d", &arr[i]);
+
+        if (is_palindrome(arr[i])) {
+            count++;
+        }
+    }
+
+    printf("%d", count);
+    return 0;
 }
 
-int is_palindrome(int a){
-    int a,rnum=0,rem,num;
-   scanf("%d",&a);
-     a=num;
-    while(a>0){
-        rem=a%10;
-        rnum=rnum*10+rem;
-        num/=10;
+// Function to check if a number is a palindrome
+int is_palindrome(int a) {
+    int rnum = 0, rem, num = a;  // Store original number
+
+    while (a > 0) {
+        rem = a % 10;
+        rnum = rnum * 10 + rem;
+        a /= 10;
     }
-        if(rnum==num){
-            return 0;
-        }
-        else{
-            return 1;
-        }
-    return 0;
+
+    return (rnum == num);  // Returns 1 if palindrome, 0 otherwise
 }
