0514e7997f3fb0bac574ed4d7bc43509666f7446
Muskan Gera <muskangera411@gmail.com>
2025-03-11 18:35:47 +0530

    Solution to Find the Maximum Product of Two Elements in an Array

---
+++ Find the Maximum Product of Two Elements in an Array@@676c02002dbc8c3e7a280b80/code.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+    int a;
+    scanf("%d",&a);
+    int arr[a];
+    for(int i=0;i<a;i++){
+        scanf("%d",arr[i]);
+    }
+    int max_product=0;
+    for(int i=0;i<a;i++){
+        for(int j=1;j<a;j++){
+            if(i*j>max_product){
+                max_product=i*j;
+            }
+
+        }
+    }
+    printf("%d",max_product);
+    return 0;
+}
